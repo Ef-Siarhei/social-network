8a51be312a24e9bad636f8ddff6544d4d8c99410
Siarhei <h1_n1@mail.ru>
2024-02-27 17:00:20 +0300

    feat: add message to props

--- src/components/profile/MyPosts/MyPosts.jsx
+++ src/components/profile/MyPosts/MyPosts.jsx
@@ -1,14 +1,14 @@
 import p from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-export default function MyPosts() {
+export default function MyPosts(props) {
   return (
     <div>
       My posts
       <div>New post</div>
       <div className={p.posts}>
-        <Post />
-        <Post />
+        <Post message="Hi, how are you?" like="5" />
+        <Post message="It's my first post." like="20" />
       </div>
     </div>
   );
