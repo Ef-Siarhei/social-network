87c9975edfed741fee701a41f7bc5dc6ea63d445
Siarhei <h1_n1@mail.ru>
2024-02-28 11:01:02 +0300

    feat: add link to NavBar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,11 +4,21 @@ export default function Navbar() {
   return (
     <nav className={n.nav}>
       <ul>
-        <li className={n.item}>Profile</li>
-        <li className={n.item}>Messages</li>
-        <li className={n.item}>News</li>
-        <li className={n.item}>Music</li>
-        <li className={n.item}>Settings</li>
+        <li className={n.item}>
+          <a href="/profile">Profile</a>
+        </li>
+        <li className={n.item}>
+          <a href="dialogs">Messages</a>
+        </li>
+        <li className={n.item}>
+          <a href="news">News</a>
+        </li>
+        <li className={n.item}>
+          <a href="music">Music</a>
+        </li>
+        <li className={n.item}>
+          <a href="settings">Settings</a>
+        </li>
       </ul>
     </nav>
   );
