7ff454107b0512e53618d8ae3e9c1bedba5a212d
Siarhei Yafimenka <h1_n1@mail.ru>
2024-03-17 18:19:16 +0300

    feat: add withRoute and useParams ??????????????????????

--- src/components/profile/ProfileContainer.jsx
+++ src/components/profile/ProfileContainer.jsx
@@ -4,11 +4,28 @@ import Profile from './Profile';
 import axios from 'axios';
 import Preloader from '../comoon/Preloader/Preloafer';
 import { setUserProfile } from '../../redux/reduced/profile-reducer';
+import { useParams } from 'react-router-dom';
+
+const withRouter = (WrappedComponent) => (props) => {
+  const params = useParams();
+  // etc... other react-router-dom v6 hooks
+  return (
+    <WrappedComponent
+      {...props}
+      params={params}
+      // etc...
+    />
+  );
+};
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
+    let userId = this.props.params.userId;
+    if (!userId) {
+      userId = 2;
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then((response) => {
         this.props.setUserProfile(response.data);
       });
@@ -28,6 +45,8 @@ const mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
 
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
+
 export default connect(mapStateToProps, {
   setUserProfile,
-})(ProfileContainer);
+})(WithUrlDataContainerComponent);
