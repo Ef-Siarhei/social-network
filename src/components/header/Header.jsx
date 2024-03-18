884c551b956373cfdf9735be6ec216bf47301598
Siarhei <h1_n1@mail.ru>
2024-03-18 16:04:35 +0300

    feat: add icon to user login

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import s from './Header.module.css';
 import { NavLink } from 'react-router-dom';
+import UserIcon from '../comoon/UserIcon/UserIcon';
 
 export default function Header(props) {
   return (
@@ -9,7 +10,14 @@ export default function Header(props) {
         alt=""
       />
       <div className={s.loginBlock}>
-        {props.isAuth ? props.login : <NavLink to="/login">Login</NavLink>}
+        {props.isAuth ? (
+          <>
+            <UserIcon img={null} />
+            {props.login}
+          </>
+        ) : (
+          <NavLink to="/login">Login</NavLink>
+        )}
       </div>
     </header>
   );
