d6fd6d076198a32ed9df24e586f8ec3d9055a115
Siarhei <h1_n1@mail.ru>
2024-03-19 15:08:15 +0300

    refactor: use baseUrl

--- src/api/api.js
+++ src/api/api.js
@@ -1,23 +1,19 @@
 import axios from 'axios';
 
+const baseUrl = 'https://social-network.samuraijs.com/api/1.0/';
+
 export const getUsers = (currentPage = 1, pageSize = 10) => {
   return axios
-    .get(
-      `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-      {
-        withCredentials: true,
-      },
-    )
+    .get(baseUrl + `users?page=${currentPage}&count=${pageSize}`, {
+      withCredentials: true,
+    })
     .then((response) => response.data);
 };
 
 export const getUsersClickToNumberPage = (pageNumber, pageSize) => {
   return axios
-    .get(
-      `https://social-network.samuraijs.com/api/1.0/users?count=${pageSize}&page=${pageNumber}`,
-      {
-        withCredentials: true,
-      },
-    )
+    .get(baseUrl + `users?count=${pageSize}&page=${pageNumber}`, {
+      withCredentials: true,
+    })
     .then((response) => response.data);
 };
