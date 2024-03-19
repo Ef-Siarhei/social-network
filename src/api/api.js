5af805e177fdb2ef1c11c3b49b5e4bbc58ae67b0
Siarhei <h1_n1@mail.ru>
2024-03-19 15:31:53 +0300

    feat: add instance from axios.create api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,22 @@
 import axios from 'axios';
 
-const baseUrl = 'https://social-network.samuraijs.com/api/1.0/';
+const instance = axios.create({
+  withCredentials: true,
+  // work without key!!!
+  // headers: {
+  //   'API-KEY': '59a8c29a-9be7-4874-919c-a8a3bc4e3776',
+  // },
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+});
 
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-  return axios
-    .get(baseUrl + `users?page=${currentPage}&count=${pageSize}`, {
-      withCredentials: true,
-    })
+  return instance
+    .get(`users?page=${currentPage}&count=${pageSize}`)
     .then((response) => response.data);
 };
 
 export const getUsersClickToNumberPage = (pageNumber, pageSize) => {
-  return axios
-    .get(baseUrl + `users?count=${pageSize}&page=${pageNumber}`, {
-      withCredentials: true,
-    })
+  return instance
+    .get(`users?count=${pageSize}&page=${pageNumber}`)
     .then((response) => response.data);
 };
