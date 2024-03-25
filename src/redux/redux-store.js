dc5f5a007bff4e0856db8ef0ca53547ace747462
Siarhei <h1_n1@mail.ru>
2024-03-25 16:43:29 +0300

    feat: add form login

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -9,6 +9,7 @@ import sidebarReducer from './reduced/sidebar-reducer';
 import usersReducer from './reduced/users-reducer';
 import authReducer from './reduced/auth-reducer';
 import { thunk as thunkMiddleware } from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
 // createStore устарел потому legacy_createStore as createStore
 
 let reducers = combineReducers({
@@ -17,6 +18,7 @@ let reducers = combineReducers({
   sidebar: sidebarReducer,
   usersPage: usersReducer,
   auth: authReducer,
+  form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
