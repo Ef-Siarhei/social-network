77795bde1542383b92a0ae0f4e65790320093a03
Siarhei Yafimenka <h1_n1@mail.ru>
2024-03-25 06:28:50 +0300

    feat: http put with use onChange for input value

--- src/components/profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
+++ src/components/profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -1,80 +1,90 @@
 import React, { useState } from 'react';
 
-class ProfileStatus extends React.Component {
-  statusInputRef = React.createRef();
-  state = {
-    editMode: false,
-  };
-  activateEditMode = () => {
-    console.log(this);
-    this.setState({
-      editMode: true,
-    });
-  };
-  deactivateEditMode = () => {
-    this.setState({
-      editMode: false,
-    });
-    this.props.updateUserStatus(this.statusInputRef.current.value);
-  };
-
-  render() {
-    return (
-      <div>
-        {!this.state.editMode ? (
-          <div>
-            <span onDoubleClick={this.activateEditMode}>
-              {this.props.status}
-            </span>
-          </div>
-        ) : (
-          <div>
-            <input
-              ref={this.statusInputRef}
-              autoFocus={true}
-              onBlur={this.deactivateEditMode}
-              type={'text'}
-              defaultValue={this.props.status}
-            />
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-export default ProfileStatus;
+// class ProfileStatus extends React.Component {
+//   state = {
+//     editMode: false,
+//     status: this.props.status,
+//   };
+//   activateEditMode = () => {
+//     console.log(this);
+//     this.setState({
+//       editMode: true,
+//     });
+//   };
+//   deactivateEditMode = () => {
+//     this.setState({
+//       editMode: false,
+//     });
+//     this.props.updateUserStatus(this.state.status);
+//   };
+//
+//   onStatusChange = (e) => {
+//     this.setState({
+//       status: e.currentTarget.value,
+//     });
+//   };
+//
+//   render() {
+//     return (
+//       <div>
+//         {!this.state.editMode ? (
+//           <div>
+//             <span onDoubleClick={this.activateEditMode}>
+//               {this.props.status || '-----'}
+//             </span>
+//           </div>
+//         ) : (
+//           <div>
+//             <input
+//               onChange={this.onStatusChange}
+//               autoFocus={true}
+//               onBlur={this.deactivateEditMode}
+//               type={'text'}
+//               value={this.state.status}
+//             />
+//           </div>
+//         )}
+//       </div>
+//     );
+//   }
+// }
+// export default ProfileStatus;
 
 // This also worked
-// const ProfileStatus = (props) => {
-//   let [state, setState] = useState(false);
-//   let statusInputRef = React.createRef();
-//
-//   const activateEditMode = () => {
-//     setState(true);
-//   };
-//   const deactivateEditMode = () => {
-//     setState(false);
-//     props.updateUserStatus(statusInputRef.current.value);
-//   };
-//
-//   return (
-//     <div>
-//       {!state ? (
-//         <div>
-//           <span onDoubleClick={activateEditMode}>{props.status}</span>
-//         </div>
-//       ) : (
-//         <div>
-//           <input
-//             ref={statusInputRef}
-//             autoFocus={true}
-//             onBlur={deactivateEditMode}
-//             type={'text'}
-//             defaultValue={props.status}
-//           />
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-// export default ProfileStatus;
+const ProfileStatus = (props) => {
+  let [editMode, setEditMode] = useState(false);
+  let [status, setStatus] = useState(props.status);
+
+  const activateEditMode = () => {
+    setEditMode(true);
+  };
+  const deactivateEditMode = () => {
+    setEditMode(false);
+    props.updateUserStatus(status);
+  };
+
+  const onStatusChange = (e) => {
+    setStatus(e.currentTarget.value);
+  };
+
+  return (
+    <div>
+      {!editMode ? (
+        <div>
+          <span onDoubleClick={activateEditMode}>{props.status || '----'}</span>
+        </div>
+      ) : (
+        <div>
+          <input
+            onChange={onStatusChange}
+            autoFocus={true}
+            onBlur={deactivateEditMode}
+            type={'text'}
+            value={status}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+export default ProfileStatus;
