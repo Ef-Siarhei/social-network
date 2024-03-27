ead0393af9c6df36df3fb2c95ce9c8838bc2ccdd
Siarhei Yafimenka <h1_n1@mail.ru>
2024-03-27 23:17:48 +0300

    feat: add validator for login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,27 @@
 import { Field, reduxForm } from 'redux-form';
+import { Input } from '../comoon/FormsControl/FormsControl';
+import { maxLengthCreator, required } from '../../utils/validators/validators';
+
+const maxLength10 = maxLengthCreator(10);
 
 const LoginForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit}>
       <div>
-        <Field placeholder={'Login'} name={'login'} component={'input'} />
+        <Field
+          component={Input}
+          name={'login'}
+          placeholder={'Login'}
+          validate={[required, maxLength10]}
+        />
       </div>
       <div>
-        <Field placeholder={'Password'} name={'password'} component={'input'} />
+        <Field
+          component={Input}
+          name={'password'}
+          placeholder={'Password'}
+          validate={[required, maxLength10]}
+        />
       </div>
       <div>
         <Field type={'checkbox'} name={'rememberMe'} component={'input'} />{' '}
