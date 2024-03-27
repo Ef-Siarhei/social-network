355c198277681baef831826391a6c307ec5f49cb
Siarhei Yafimenka <h1_n1@mail.ru>
2024-03-27 21:24:45 +0300

    feat: add validator for dialogs newMessage

--- src/components/dialogs/Message/NewMessage/NewMessage.jsx
+++ src/components/dialogs/Message/NewMessage/NewMessage.jsx
@@ -1,14 +1,22 @@
 import { Field, reduxForm } from 'redux-form';
 import s from './NewMessage.module.css';
+import { Textarea } from '../../../comoon/FormsControl/FormsControl';
+import {
+  maxLengthCreator,
+  required,
+} from '../../../../utils/validators/validators';
+
+const maxLength20 = maxLengthCreator(20);
 
 const addMessageForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit} className={s.newMessage}>
       <Field
-        component={'textarea'}
         name={'message'}
+        component={Textarea}
         placeholder={'Enter your message'}
         className={s.input}
+        validate={[required, maxLength20]}
       />
       <button>Send message</button>
     </form>
