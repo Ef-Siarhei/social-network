190c73296cc3a56cc7d43cd586cd605399f2d163
Siarhei Yafimenka <h1_n1@mail.ru>
2024-04-02 21:53:10 +0300

    feat: use reselect in ProfileContainer for status

--- src/redux/selectors/profile-selectors.js
+++ src/redux/selectors/profile-selectors.js
@@ -1,4 +1,7 @@
+import {createSelector} from "reselect";
+
 export const getProfile = (state) => state.profilePage.profile;
-export const getStatus = (state) => state.profilePage.status;
+export const getStatusPrimitiveSelector = (state) => state.profilePage.status;
+export const getStatus = createSelector([getStatusPrimitiveSelector], (status) => status);
 export const getAuthorizedUserId = (state) => state.auth.id;
 export const getIsAuth = (state) => state.auth.isAuth;
