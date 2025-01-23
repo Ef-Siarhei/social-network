34f9cd05bd5653fc285db442b909b92d24b9a00f
Siarhei Yafimenka <h1_n1@mail.ru>
2025-01-23 17:35:44 +0300

    migration users-selectors.js file to ts

---
+++ src/redux/selectors/users-selectors.ts
@@ -0,0 +1,25 @@
+import {createSelector} from "reselect";
+import {AppStateType} from "../redux-store";
+
+const getUsersPrimitiveSelector = (state: AppStateType) => {
+  return state.usersPage.users;
+};
+export const getUsers = createSelector([getUsersPrimitiveSelector], (users) => {
+  return users.filter(user => true);
+})
+
+export const getPageSize = (state: AppStateType) => {
+  return state.usersPage.pageSize;
+};
+export const getCurrentPage = (state: AppStateType) => {
+  return state.usersPage.currentPage;
+};
+export const getTotalUsersCount = (state: AppStateType) => {
+  return state.usersPage.totalUsersCount;
+};
+export const getIsFetching = (state: AppStateType) => {
+  return state.usersPage.isFetching;
+};
+export const getFollowingIsProgress = (state: AppStateType) => {
+  return state.usersPage.followingIsProgress;
+};
