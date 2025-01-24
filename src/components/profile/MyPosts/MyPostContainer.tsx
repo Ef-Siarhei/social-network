d100f7b06f7aed6f28c4b973499bcd4e1912725c
Siarhei Yafimenka <h1_n1@mail.ru>
2025-01-24 22:34:40 +0300

    typified file MyPostContainer.tsx

---
+++ src/components/profile/MyPosts/MyPostContainer.tsx
@@ -0,0 +1,20 @@
+import MyPosts from './MyPosts'
+import { addPost } from '../../../redux/reduced/profile-reducer'
+import { connect } from 'react-redux'
+import {AppStateType} from "../../../redux/redux-store"
+import {PostType} from "../../../types/types"
+import React from "react"
+
+type MapStateToPropsType = {
+  posts: Array<PostType>
+}
+
+let mapStateToProps = (state: AppStateType): MapStateToPropsType => {
+  return {
+    posts: state.profilePage.posts,
+  };
+};
+
+const MyPostContainer = connect(mapStateToProps, { addPost })(MyPosts)
+
+export default MyPostContainer;
