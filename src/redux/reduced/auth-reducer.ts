57aa018338f15d1cb8c99bb561270e038b370765
Siarhei Yafimenka <h1_n1@mail.ru>
2025-02-03 17:35:29 +0300

    output the automatic type InitialStateType with help typeof & add to action type 'auth/'

--- src/redux/reduced/auth-reducer.ts
+++ src/redux/reduced/auth-reducer.ts
@@ -5,28 +5,21 @@ import {AppStateType, InferActionsTypes} from "../redux-store"
 import {authAPI} from "../../api/auth-api";
 import {securityApi} from "../../api/security-api";
 
-type InitialStateType = {
-  id: number | null
-  email: string | null
-  login: string | null
-  isAuth: boolean
-  captchaUrl: string | null
-}
-
-type ActionsTypes = InferActionsTypes<typeof actions>
-
-let initialState: InitialStateType = {
-  id: null,
-  email: null,
-  login: null,
+let initialState = {
+  id: null as (number | null),
+  email: null as (string | null),
+  login: null as (string | null),
   isAuth: false,
-  captchaUrl: null,
+  captchaUrl: null as (string | null),
 }
+
+type InitialStateType = typeof initialState
+type ActionsTypes = InferActionsTypes<typeof actions>
 
 const authReducer = (state = initialState, action: ActionsTypes): InitialStateType => {
   switch (action.type) {
-    case 'SET_USER_DATA':
-    case 'GET_CAPTCHA_URL_SUCCESS': {
+    case 'auth/SET_USER_DATA':
+    case 'auth/GET_CAPTCHA_URL_SUCCESS': {
       return {
         ...state,
         ...action.payload,
@@ -40,11 +33,11 @@ const authReducer = (state = initialState, action: ActionsTypes): InitialStateTy
 // Actions creator
 const actions = {
   setAuthUserData: (id: number | null, email: string | null, login: string | null, isAuth: boolean) => ({
-    type: 'SET_USER_DATA',
+    type: 'auth/SET_USER_DATA',
     payload: {id, email, login, isAuth},
   } as const),
   getCaptchaUrlSuccess: (captchaUrl: string) => ({
-    type: 'GET_CAPTCHA_URL_SUCCESS',
+    type: 'auth/GET_CAPTCHA_URL_SUCCESS',
     payload: {captchaUrl},
   } as const)
 }
