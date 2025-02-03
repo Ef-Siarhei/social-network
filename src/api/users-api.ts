c3c82ad5b5202faf81eeb8eb8c01ac51ba695bca
Siarhei Yafimenka <h1_n1@mail.ru>
2025-02-03 15:40:49 +0300

    typified users-api as my teacher

--- src/api/users-api.ts
+++ src/api/users-api.ts
@@ -1,14 +1,11 @@
 import {UserType} from "../types/types";
-import {instance} from "./api";
-import {LogOutResponseType} from "./auth-api";
+import {instance, ResponseType} from "./api";
 
 type GetUsersResponseType = {
   items: Array<UserType>
   totalCount: number
   error: string
 }
-
-export type FollowUnFollowResponseType = LogOutResponseType
 
 export const usersAPI = {
   getUsers(currentPage = 1, pageSize = 10) {
@@ -18,10 +15,10 @@ export const usersAPI = {
   },
 
   follow(id: number) {
-    return instance.post<FollowUnFollowResponseType>(`follow/${id}`).then((response) => response.data)
+    return instance.post<ResponseType>(`follow/${id}`).then((response) => response.data)
   },
 
   unFollow(id: number) {
-    return instance.delete<FollowUnFollowResponseType>(`follow/${id}`).then((response) => response.data)
+    return instance.delete<ResponseType>(`follow/${id}`).then((response) => response.data)
   },
 }
--- src/redux/reduced/users-reducer.ts
+++ src/redux/reduced/users-reducer.ts
@@ -1,10 +1,10 @@
-import {ResultCodesEnum} from '../../api/api'
+import {ResultCodesEnum, ResponseType} from '../../api/api'
 import {updateObjectInArray} from '../../utils/object-helper'
 import {UserType} from "../../types/types"
 import {ThunkAction} from "redux-thunk"
 import {AppStateType, InferActionsTypes} from "../redux-store"
 import {Dispatch} from "redux"
-import {FollowUnFollowResponseType, usersAPI} from "../../api/users-api";
+import {usersAPI} from "../../api/users-api";
 
 const initialState = {
   users: [] as Array<UserType>,
@@ -107,7 +107,7 @@ export const requestUsers = (currentPage: number, pageSize: number): ThunkType =
 const _followUnFollowFlow = async (
   dispatch: DispatchType,
   userId: number,
-  apiMethod: (userId: number) => Promise<FollowUnFollowResponseType>,
+  apiMethod: (userId: number) => Promise<ResponseType>,
   actionCreator: (userId: number) => ActionTypes,
 ) => {
   dispatch(actions.toggleFollowingProgress(true, userId))
