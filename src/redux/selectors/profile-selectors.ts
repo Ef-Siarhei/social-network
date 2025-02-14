b6b886a41c4069e1a909c099aa462c5677ba3389
Siarhei Yafimenka <h1_n1@mail.ru>
2025-02-14 17:21:47 +0300

    fix mistake browser about use createSelector

--- src/redux/selectors/profile-selectors.ts
+++ src/redux/selectors/profile-selectors.ts
@@ -1,10 +1,7 @@
-import {createSelector} from "reselect";
 import {AppStateType} from "../redux-store";
 
 export const getProfile = (state: AppStateType) => state.profilePage.profile;
-export const getStatusPrimitiveSelector = (state: AppStateType) => state.profilePage.status;
-export const getStatus = createSelector([getStatusPrimitiveSelector], (status) => status);
+export const getStatus = (state: AppStateType) => state.profilePage.status;
 export const getAuthorizedUserId = (state: AppStateType) => state.auth.id;
-export const getProfileUpdateStatusPrimitiveSelector = (state: AppStateType) => state.profilePage.profileUpdateStatus;
-export const getProfileUpdateStatus = createSelector([getProfileUpdateStatusPrimitiveSelector], (status) => status)
+export const getProfileUpdateStatus = (state: AppStateType) => state.profilePage.profileUpdateStatus;
 export const getPosts = (state: AppStateType) => state.profilePage.posts
