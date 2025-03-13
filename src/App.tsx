ee5eceec733fd87993aef540450c20acbf494d07
Efimenko_Sergey <fima.ysa@mail.ru>
2025-03-13 11:57:22 +0300

    add NewsPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,183 +1,188 @@
-import React, {FC, lazy, Suspense, useEffect} from 'react';
-import {HashRouter, Navigate, NavLink, Route, Routes, useLocation} from 'react-router-dom';
-import './App.scss';
-import Music from './components/music/Music';
-import News from './components/news/News';
-import Settings from './components/settings/Settings';
-import {LoginPage} from './components/Login/LoginPage';
-import {Provider, useDispatch, useSelector} from 'react-redux';
-import {initializeApp, showGlobalError, unShowGlobalError} from './redux/reduced/app-reducer';
-import Preloader from './components/common/Preloader/Preloader';
-import store, {AppDispatch} from './redux/redux-store';
-import PopUpError from "./components/common/popUp/PopUpError/PopUpError";
-import {QueryParamProvider} from "use-query-params";
-import {ReactRouter6Adapter} from "use-query-params/adapters/react-router-6";
-import {getGlobalError, getInitialized} from "./redux/selectors/app-selectors";
-import {Friends} from "./components/friends/Friends";
-import {Header} from "./components/header/Header";
+import {
+	LaptopOutlined,
+	NotificationOutlined,
+	SettingOutlined,
+	UserOutlined
+} from '@ant-design/icons'
+import {Breadcrumb, Layout, Menu, MenuProps, theme} from 'antd'
+import React, {FC, lazy, Suspense, useEffect} from 'react'
+import {Provider, useDispatch, useSelector} from 'react-redux'
+import {HashRouter, Navigate, NavLink, Route, Routes, useLocation} from 'react-router-dom'
+import {QueryParamProvider} from 'use-query-params'
+import {ReactRouter6Adapter} from 'use-query-params/adapters/react-router-6'
+import './App.scss'
+import PopUpError from './components/common/popUp/PopUpError/PopUpError'
+import Preloader from './components/common/Preloader/Preloader'
+import {Friends} from './components/friends/Friends'
+import {Header} from './components/header/Header'
+import {LoginPage} from './components/Login/LoginPage'
+import Music from './components/music/Music'
+import Settings from './components/settings/Settings'
+import {initializeApp, showGlobalError, unShowGlobalError} from './redux/reduced/app-reducer'
+import store, {AppDispatch} from './redux/redux-store'
+import {getGlobalError, getInitialized} from './redux/selectors/app-selectors'
+import {selectIsAuth} from './redux/selectors/auth-selectors'
 
-import {LaptopOutlined, NotificationOutlined, SettingOutlined, UserOutlined} from '@ant-design/icons';
-import {Breadcrumb, Layout, Menu, MenuProps, theme} from 'antd';
-import {selectIsAuth} from "./redux/selectors/auth-selectors";
-
-const ProfileContainer = lazy(() => import('./components/profile/ProfileContainer'));
-const DialogsPage = lazy(() => import('./components/dialogs/DialogsPage').then(module => ({default: module.DialogsPage})));// then if export not default
-const UsersPage = lazy(() => import('./components/users/UsersContainer'));
-const ChatPage = lazy(() => import('./pages/Chat/ChatPage').then(module => ({default: module.ChatPage})));
+const ProfileContainer = lazy(() => import('./components/profile/ProfileContainer'))
+const DialogsPage = lazy(() => import('./components/dialogs/DialogsPage').then(module => ({default: module.DialogsPage})))// then if export not default
+const UsersPage = lazy(() => import('./components/users/UsersContainer'))
+const ChatPage = lazy(() => import('./pages/Chat/ChatPage').then(module => ({default: module.ChatPage})))
+const NewsPage = lazy(() => import('./pages/News/NewsPage').then(module => ({default: module.NewsPage})))
 
 // Antd -----------------------------------------------------------------------
-const {Content, Footer, Sider} = Layout;
+const {Content, Footer, Sider} = Layout
 const {SubMenu} = Menu
 
 
 const menuSidebar = [
-  {
-    label: 'Profile',
-    children: ['Profile', 'Dialogs']
-  },
-  {
-    label: 'Developers',
-    children: ['Users', 'Chat']
-  },
-  {
-    label: 'Others',
-    children: ['News', 'Music']
-  },
-  {
-    label: 'Settings',
-    children: null
-  },
+	{
+		label: 'Profile',
+		children: ['Profile', 'Dialogs']
+	},
+	{
+		label: 'Developers',
+		children: ['Users', 'Chat']
+	},
+	{
+		label: 'Others',
+		children: ['News', 'Music']
+	},
+	{
+		label: 'Settings',
+		children: null
+	}
 ]
 const sidebar: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined, SettingOutlined].map(
-  (icon, index) => {
-    const key = String(index + 1);
+	(icon, index) => {
+		const key = String(index + 1)
 
-    return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: menuSidebar[index].children ? menuSidebar[index].label :
-        <NavLink to={`/${menuSidebar[index].label.toLowerCase()}`}>{menuSidebar[index].label}</NavLink>,
+		return {
+			key: `sub${key}`,
+			icon: React.createElement(icon),
+			label: menuSidebar[index].children ? menuSidebar[index].label :
+				<NavLink
+					to={`/${menuSidebar[index].label.toLowerCase()}`}>{menuSidebar[index].label}</NavLink>,
 
-      children: menuSidebar[index].children ? [...menuSidebar[index].children].map((item) => {
-        return {
-          key: '/' + item.toLowerCase(),
-          label: <NavLink to={`/${item.toLowerCase()}`}>{item}</NavLink>,
-        };
-      }) : null
-    };
-  },
-);
+			children: menuSidebar[index].children ? [...menuSidebar[index].children].map((item) => {
+				return {
+					key: '/' + item.toLowerCase(),
+					label: <NavLink to={`/${item.toLowerCase()}`}>{item}</NavLink>
+				}
+			}) : null
+		}
+	}
+)
 
 
 const App: React.FC = () => {
-  const {
-    token: {colorBgContainer, borderRadiusLG},
-  } = theme.useToken();
+	const {
+		token: {colorBgContainer, borderRadiusLG}
+	} = theme.useToken()
 
-  const isAuth = useSelector(selectIsAuth)
-  const initialized = useSelector(getInitialized)
-  const globalError = useSelector(getGlobalError)
-  const dispatch: AppDispatch = useDispatch()
-  const location = useLocation()
+	const isAuth = useSelector(selectIsAuth)
+	const initialized = useSelector(getInitialized)
+	const globalError = useSelector(getGlobalError)
+	const dispatch: AppDispatch = useDispatch()
+	const location = useLocation()
 
-  const catchUnhandledErrors = (event: PromiseRejectionEvent) => {
-    const messageError = `Unhandled Rejection at: ${event.promise}, reason: ${event.reason}`
-    dispatch(showGlobalError(messageError))
-  }
+	const catchUnhandledErrors = (event: PromiseRejectionEvent) => {
+		const messageError = `Unhandled Rejection at: ${event.promise}, reason: ${event.reason}`
+		dispatch(showGlobalError(messageError))
+	}
 
-  const unShowMessage = () => {
-    dispatch(unShowGlobalError())
-  }
+	const unShowMessage = () => {
+		dispatch(unShowGlobalError())
+	}
 
-  useEffect(() => {
-    dispatch(initializeApp())
-    window.addEventListener('unhandledrejection', catchUnhandledErrors)
-    // If we subscribed to addEventListener in componentDidMount,
-    // we must unsubscribe from it addEventListener to componentWillUnmount.
-    // If we subscribed to addEventListener in useEffect.
-    // we must unsubscribe from it addEventListener return function in useEffect .
-    return () => {
-      window.removeEventListener('unhandledrejection', catchUnhandledErrors)
-    }
-  }, [])
+	useEffect(() => {
+		dispatch(initializeApp())
+		window.addEventListener('unhandledrejection', catchUnhandledErrors)
+		// If we subscribed to addEventListener in componentDidMount,
+		// we must unsubscribe from it addEventListener to componentWillUnmount.
+		// If we subscribed to addEventListener in useEffect.
+		// we must unsubscribe from it addEventListener return function in useEffect .
+		return () => {
+			window.removeEventListener('unhandledrejection', catchUnhandledErrors)
+		}
+	}, [])
 
-  if (!initialized) {
-    return <Preloader/>
-  }
+	if (!initialized) {
+		return <Preloader/>
+	}
 
-  return (
-    <Layout>
-      <Header/>
-      <div style={{padding: '0 48px'}}>
-        <Breadcrumb style={{margin: '16px 0'}}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <Layout
-          style={{padding: '24px 0', background: colorBgContainer, borderRadius: borderRadiusLG}}
-        >
-          <Sider style={{background: colorBgContainer}} width={200}>
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={[location.pathname]}
-              defaultOpenKeys={['sub1']}
-              style={{height: 'auto'}}
-              items={sidebar}
-            >
-              {/*<SubMenu key={'sub1'} icon={<UserOutlined/>} title={'My Profile'}>*/}
-              {/*  <Menu.Item key={'1'}><NavLink to="/profile"> Profile </NavLink></Menu.Item>*/}
-              {/*  <Menu.Item key={'2'}><NavLink to="/dialogs"> Dialogs </NavLink></Menu.Item>*/}
-              {/*</SubMenu>*/}
-              {/*<SubMenu key={'sub2'} title={'Developers'} icon={<LaptopOutlined/>}>*/}
-              {/*  <Menu.Item key={'3'}><NavLink to="/users"> Users </NavLink></Menu.Item>*/}
-              {/*</SubMenu>*/}
-              {/*  <Menu.Item key={'4'}><NavLink to="/music"> Music </NavLink></Menu.Item>*/}
-              {/*  <Menu.Item key={'5'}><NavLink to="/news"> News </NavLink></Menu.Item>*/}
-              {/*  <Menu.Item key={'6'} icon={<SettingOutlined/>}><NavLink to="/settings"> Settings </NavLink></Menu.Item>*/}
-            </Menu>
-            {isAuth ? <Friends/> : <NavLink to={'/login'}/>}
-          </Sider>
-          <Content style={{padding: '0 24px', minHeight: 280}}>
-            <Suspense fallback={<div>LOADING....</div>}>
-              <QueryParamProvider adapter={ReactRouter6Adapter}>
-                <Routes>
-                  <Route path="/" element={<Navigate to='/profile'/>}/>
-                  <Route path="/profile/:userId?" element={<ProfileContainer/>}/>
-                  <Route path="/dialogs/*" element={<DialogsPage/>}/>
-                  <Route path="/news" element={<News/>}/>
-                  <Route path="/music" element={<Music/>}/>
-                  <Route path="/users" element={<UsersPage pageTitle={'Just go ahead!!!'}/>}/>
-                  <Route path="/settings" element={<Settings/>}/>
-                  <Route path="/login" element={<LoginPage/>}/>
-                  <Route path="/chat" element={<ChatPage/>}/>
-                  <Route path="*" element={<div>404 NOT FOUND</div>}/>
-                </Routes>
-              </QueryParamProvider>
-            </Suspense>
-            {globalError &&
-              <PopUpError message={globalError} unShowMessage={unShowMessage}/>
-            }
-          </Content>
-        </Layout>
-      </div>
-      <Footer style={{textAlign: 'center'}}>
-        Efimenko Sergei ©{new Date().getFullYear()} Created by Ant UED
-      </Footer>
-    </Layout>
-  );
-};
+	return (
+		<Layout>
+			<Header/>
+			<div style={{padding: '0 48px'}}>
+				<Breadcrumb style={{margin: '16px 0'}}>
+					<Breadcrumb.Item>Home</Breadcrumb.Item>
+					<Breadcrumb.Item>List</Breadcrumb.Item>
+					<Breadcrumb.Item>App</Breadcrumb.Item>
+				</Breadcrumb>
+				<Layout
+					style={{padding: '24px 0', background: colorBgContainer, borderRadius: borderRadiusLG}}
+				>
+					<Sider style={{background: colorBgContainer}} width={200}>
+						<Menu
+							mode='inline'
+							defaultSelectedKeys={[location.pathname]}
+							defaultOpenKeys={['sub1']}
+							style={{height: 'auto'}}
+							items={sidebar}
+						>
+							{/*<SubMenu key={'sub1'} icon={<UserOutlined/>} title={'My Profile'}>*/}
+							{/*  <Menu.Item key={'1'}><NavLink to="/profile"> Profile </NavLink></Menu.Item>*/}
+							{/*  <Menu.Item key={'2'}><NavLink to="/dialogs"> Dialogs </NavLink></Menu.Item>*/}
+							{/*</SubMenu>*/}
+							{/*<SubMenu key={'sub2'} title={'Developers'} icon={<LaptopOutlined/>}>*/}
+							{/*  <Menu.Item key={'3'}><NavLink to="/users"> Users </NavLink></Menu.Item>*/}
+							{/*</SubMenu>*/}
+							{/*  <Menu.Item key={'4'}><NavLink to="/music"> Music </NavLink></Menu.Item>*/}
+							{/*  <Menu.Item key={'5'}><NavLink to="/news"> News </NavLink></Menu.Item>*/}
+							{/*  <Menu.Item key={'6'} icon={<SettingOutlined/>}><NavLink to="/settings"> Settings </NavLink></Menu.Item>*/}
+						</Menu>
+						{isAuth ? <Friends/> : <NavLink to={'/login'}/>}
+					</Sider>
+					<Content style={{padding: '0 24px', minHeight: 280}}>
+						<Suspense fallback={<div>LOADING....</div>}>
+							<QueryParamProvider adapter={ReactRouter6Adapter}>
+								<Routes>
+									<Route path='/' element={<Navigate to='/profile'/>}/>
+									<Route path='/profile/:userId?' element={<ProfileContainer/>}/>
+									<Route path='/dialogs/*' element={<DialogsPage/>}/>
+									<Route path='/news' element={<NewsPage/>}/>
+									<Route path='/music' element={<Music/>}/>
+									<Route path='/users' element={<UsersPage pageTitle={'Just go ahead!!!'}/>}/>
+									<Route path='/settings' element={<Settings/>}/>
+									<Route path='/login' element={<LoginPage/>}/>
+									<Route path='/chat' element={<ChatPage/>}/>
+									<Route path='*' element={<div>404 NOT FOUND</div>}/>
+								</Routes>
+							</QueryParamProvider>
+						</Suspense>
+						{globalError &&
+							<PopUpError message={globalError} unShowMessage={unShowMessage}/>
+						}
+					</Content>
+				</Layout>
+			</div>
+			<Footer style={{textAlign: 'center'}}>
+				Efimenko Sergei ©{new Date().getFullYear()} Created by Ant UED
+			</Footer>
+		</Layout>
+	)
+}
 // Antd -----------------------------------------------------------------------
 
 const SamuraiJSApp: FC = () => {
-  return (
-    // <React.StrictMode>
-    <HashRouter>
-      <Provider store={store}>
-        <App/>
-      </Provider>
-    </HashRouter>
-    // </React.StrictMode>
-  );
-};
-export default SamuraiJSApp;
+	return (
+		// <React.StrictMode>
+		<HashRouter>
+			<Provider store={store}>
+				<App/>
+			</Provider>
+		</HashRouter>
+		// </React.StrictMode>
+	)
+}
+export default SamuraiJSApp
---
+++ src/pages/News/NewsPage.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react'
+import s from './News.module.css'
+
+
+export const NewsPage: FC = () => {
+	return (
+		<>
+			<div className={s.news}>
+				NEWS
+			</div>
+		</>
+	)
+}
